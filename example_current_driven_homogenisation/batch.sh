c20811f2d24f330658ede65690dcaef10badf0cf
Filip Dominec <filip.dominec@gmail.com>
2015-02-27 12:33:51 +0100

    correct CDH batch file

--- example_current_driven_homogenisation/batch.sh
+++ example_current_driven_homogenisation/batch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-for Kz in `seq 0    5000 150000`; do np ../cdh.py Kz=$Kz simtime=30p zsize=100u; done
-../plot_cdh.py cdh/*
-for Kz in `seq 2500 5000 150000`; do np ../cdh.py Kz=$Kz simtime=30p zsize=100u; done
-../plot_cdh.py cdh/*
+for Kz in `seq 0    5000 150000`; do ../cdh.py Kz=$Kz simtime=30p; done
+../plot_cdh.py S*dat ## preview
+for Kz in `seq 2500 5000 150000`; do ../cdh.py Kz=$Kz simtime=30p; done
+../plot_cdh.py S*dat
 
