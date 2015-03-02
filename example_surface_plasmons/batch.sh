3832b10e357bd91ae0c4bd9dfa82f2bb37c5e655
Filip Dominec <filip.dominec@gmail.com>
2015-03-02 10:08:15 +0100

    fixes

--- example_surface_plasmons/batch.sh
+++ example_surface_plasmons/batch.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-mpirun -np 1 ../plasmons.py comment=Asym metalthick=100n resolution=50n size_x=8e-6 apdistx=6e-6 simtime=70f
+#mpirun -np 1 ../plasmons.py comment=Asym metalthick=100n resolution=50n size_x=8e-6 apdistx=6e-6 simtime=70f
+mpirun -np 1 ../plasmons.py comment=Asym metalthick=100n resolution=50n size_x=8e-6 apdistx=6e-6 simtime=20f
