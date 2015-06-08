4f96a2d6d1d9f9727c3597d1e12e3306e5e7c480
Filip Dominec <filip.dominec@gmail.com>
2015-06-08 16:25:01 +0200

    sch

--- example_metamaterial_s_parameters/sphereTiO2_LossScan/batch.sh
+++ example_metamaterial_s_parameters/sphereTiO2_LossScan/batch.sh
@@ -3,6 +3,6 @@
 par='model=SphereArray resolution=4u simtime=100p'
 
 for loss in 0.001 0.01 0.03 0.1 0.3 1; do 
-    mpirun -np 1   python ../../scatter.py $par loss=$loss; ../../effparam.py
+    mpirun -np 2   python ../../scatter.py $par loss=$loss; ../../effparam.py
 done
 ../../plot_multiline.py effparam/SphereArray_simtime\=1.000e-10_*.dat --paramname loss --paramlabel 'Losses percentage %d' --paramunit .01  --ycol 'real N' --xunit 1e12
