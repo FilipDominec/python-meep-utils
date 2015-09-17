696d8adfc83cab096611da51d94022bd7ca7fe09
Filip Dominec <filip.dominec@gmail.com>
2015-09-17 14:46:54 +0200

    adapted bash script to change in multiplot

--- example_metamaterial_s_parameters/sphereTiO2_LossScan/batch.sh
+++ example_metamaterial_s_parameters/sphereTiO2_LossScan/batch.sh
@@ -5,4 +5,4 @@ par='model=SphereArray resolution=4u simtime=100p'
 for loss in 0.001 0.01 0.03 0.1 0.3 1; do 
     mpirun -np $np   python ../../scatter.py $par loss=$loss; ../../effparam.py
 done
-../../plot_multiline.py effparam/SphereArray_simtime\=1.000e-10_*.dat --paramname loss --paramlabel 'Losses percentage %d' --paramunit .01  --ycol 'real N' --xunit 1e12
+../../plot_multiline.py effparam/SphereArray_simtime\=1.000e-10_*.dat --paramname loss --paramlabel 'Losses percentage %d' --paramunit .01  --ycol 'real N' --xlabel 'Frequency [THz]' --xeval x/1e12
