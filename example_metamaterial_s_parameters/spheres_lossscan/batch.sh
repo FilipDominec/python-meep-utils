845d12e97beb048f296d54de1e4b2cda896c240d
Filip Dominec <filip.dominec@gmail.com>
2015-10-13 16:47:34 +0200

    ENH partial switch to effparam plotting using multiplot

--- example_metamaterial_s_parameters/spheres_lossscan/batch.sh
+++ example_metamaterial_s_parameters/spheres_lossscan/batch.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
-if [ -z $NP ] ; then NP=1 ; fi			 # number of processors
+if [ -z $NP ] ; then NP=2 ; fi			 # number of processors
 par='model=SphereArray resolution=4u simtime=100p'
 
-for loss in 0.001 0.01 0.03 0.1 0.3 1; do 
+for loss in 0.003 0.01 0.03 0.1 0.3 1; do 
     mpirun -np $np   python ../../scatter.py $par loss=$loss; ../../effparam.py
 done
+
+../effparam.py
+
 ../../plot_multiline.py effparam/SphereArray_simtime\=1.000e-10_*.dat --paramname loss --paramlabel 'Losses percentage %d' --paramunit .01  --ycol 'real N' --xlabel 'Frequency [THz]' --xeval x/1e12
+
+../../plot_multiline.py effparam/RodArray_effparam.dat    --paramname radius --paramlabel none \
+        --xlabel "Frequency (THz)" --xeval x/1e12  \
+        --ycol '|r|' --ylabel 'Reflectance   $|r|$' --figsizey 2 --output ${PWD##*/}_r.pdf --color RdYlBu
+
+../../plot_multiline.py effparam/RodArray_effparam.dat    --paramname radius --paramlabel none  \
+        --xlabel "Frequency (THz)" --xeval x/1e12  \
+        --ycol '|t|' --ylabel 'Transmittance $|t|$' --figsizey 2 --output ${PWD##*/}_t.pdf --color RdYlBu_r
+
+../../plot_multiline.py effparam/RodArray_effparam.dat    --paramname radius --paramlabel none  \
+        --xlabel "Frequency (THz)" --xeval x/1e12  \
+        --ycol 'real N' --ylim2 5.  --ylabel 'Effective index $N^\prime$' --figsizey 2 --output ${PWD##*/}_n.pdf --color PiYG_r \
+		--overlayplot "2.998e8/4./50e-6/(x*1e12)"
+
--- example_metamaterial_s_parameters/test/batch.sh
+++ example_metamaterial_s_parameters/test/batch.sh
@@ -1,29 +1,25 @@
-#!/bin/batch
-if [ -z $NP ] ; then NP=2 ; fi			 # number of processors
+#!/bin/bash
+if [ -z $NP ] ; then NP=2 ; fi             # number of processors
 model=SphereArray
 cellsize=50e-6
-mpirun -np $NP ../scatter.py model=SphereArray resolution=4u simtime=100p wirethick=10u cellsize=$cellsize padding=20e-6 radius=13e-6
-../../effparam.py 
-../../plot_multiline.py RodArray.dat    --paramname radius --paramlabel '' --ycol '|r|' --contours no  \
-                    --yeval 'np.abs(y)' --ylim1 0 --ylim2 1 \
-                    --xlabel 'Normalized grating period $\Lambda/\lambda$' --xeval x*1e-9/0.8e-6 --xlim1=0    \
-                    --paramlabel 'Reflectance $\muup$m)' --parameval param*1e-9/1e-6  \
-                    --title "Reflectance $|r|^2$ of ${comment%%-narrowfreq}$\\perp$ at $\\lambda$=800 nm)" --usetex yes --colormap 'Paired/2' \
-                    --output ${PWD##*/}_r.png
+#mpirun -np $NP ../../scatter.py model=SphereArray resolution=4u simtime=50p wirethick=10u cellsize=$cellsize padding=0e-6 radius=13e-6
+mpirun -np $NP ../../scatter.py model=Slab fillfraction=1 resolution=5u simtime=50p cellsize=$cellsize padding=100e-6 epsilon=49
 
-../effparam.py
+../../effparam.py
 
-../../plot_multiline.py effparam/RodArray_effparam.dat    --paramname radius --paramlabel none \
-        --xlabel "Frequency (THz)" --xeval x/1e12  \
-        --ycol '|r|' --ylabel 'Reflectance   $|r|$' --figsizey 2 --output ${PWD##*/}_r.pdf --color RdYlBu
+sharedoptions='effparam/*.dat --paramname radius --paramlabel none --figsizey 2 --xeval x/1e12'
 
-../../plot_multiline.py effparam/RodArray_effparam.dat    --paramname radius --paramlabel none  \
-        --xlabel "Frequency (THz)" --xeval x/1e12  \
-        --ycol '|t|' --ylabel 'Transmittance $|t|$' --figsizey 2 --output ${PWD##*/}_t.pdf --color RdYlBu_r
+../../plot_multiline.py $sharedoptions --xlabel "Frequency (THz)" --ycol '|r|' \
+   	--ylabel 'Reflectance   $|r|$' --output ${PWD##*/}_r.pdf --color RdYlBu
 
-../../plot_multiline.py effparam/RodArray_effparam.dat    --paramname radius --paramlabel none  \
-        --xlabel "Frequency (THz)" --xeval x/1e12  \
-        --ycol 'real N' --ylim2 5.  --ylabel 'Effective index $N^\prime$' --figsizey 2 --output ${PWD##*/}_n.pdf --color PiYG_r \
-		--overlayplot "2.998e8/4./50e-6/(x*1e12)"
+../../plot_multiline.py $sharedoptions --xlabel "Frequency (THz)" --ycol '|t|' \
+   	--ylabel 'Transmittance $|t|$' --figsizey 2 --output ${PWD##*/}_t.pdf --color RdYlBu_r
+
+../../plot_multiline.py $sharedoptions --xlabel "Frequency (THz)" --ycol 'real N' --ylim2 5. \
+   	--ylabel 'Refractive index $N_{\text{eff}}^\prime$' --output ${PWD##*/}_nr.pdf --color PiYG_r \
+    --overlayplot "2.998e8/4./${cellsize}/(x*1e12)"
+
+../../plot_multiline.py $sharedoptions --xlabel "Frequency (THz)" --ycol 'imag N' --ylim2 5. \
+   	--ylabel 'Refractive index $N_{\text{eff}}^{\prime\prime}$' --output ${PWD##*/}_ni.pdf --color PiYG_r
 
 
