c53463699262d17bc01720dde95bafb62f5d886b
Filip Dominec <filip.dominec@gmail.com>
2016-01-25 10:20:38 +0100

    publication-ready plot

--- example_metamaterial_s_parameters/erods_acomparison-loLoss/batch.sh
+++ example_metamaterial_s_parameters/erods_acomparison-loLoss/batch.sh
@@ -2,7 +2,7 @@
 if [ -z $NP ] ; then NP=2 ; fi             # number of processors
 thz=1e12
 cellsize=100e-6
-par="resolution=1u simtime=50p loss=.1"
+par="resolution=1u simtime=150p loss=.1"
 if [ -z "$skipsimulation" ]; then 
 	for a in 80 90 120; do
 		mpirun -np $NP ../../scatter.py $par model=RodArray orientation=E radius=10e-6  cellsize=${a}u 
@@ -22,7 +22,7 @@ sharedoptions="effparam/*.dat --paramname cellsize  --parameval param*1e6 --figs
 --ylabel 'Refractive index $N_{\text{eff}}$' --output ${PWD##*/}_n.pdf 
     #--overlayplot "c/2/$cellsize/x/$thz,2*c/2/$cellsize/x/$thz,3*c/2/$cellsize/x/$thz,4*c/2/$cellsize/x/$thz" 
 
-../../plot_multiline.py $sharedoptions --paramlabel '$a$ = %.1f $\upmu$m' --xlabel "Frequency (THz)" --ycol 'real eps' --y2eval '0-y2' --ycol2 'imag eps' --ylim1 -12 --ylim2 3  \
+../../plot_multiline.py $sharedoptions --paramlabel '$a$ = %.1f $\upmu$m' --xlabel "Frequency (THz)" --ycol 'real eps' --y2eval '0-y2' --ycol2 'imag eps' --ylim1 -10 --ylim2 10  \
    	--ylabel 'Effective permittivity $\varepsilon_{\text{eff}}^{\prime}$' --output ${PWD##*/}_eps.pdf \
     --overlayplot "1-.68**2/x**2"  
 ../../plot_multiline.py $sharedoptions --paramlabel '$a$ = %.1f $\upmu$m' --xlabel "Frequency (THz)" --ycol 'real eps' --ylim1 -12 --ylim2 3  \
@@ -33,11 +33,11 @@ sharedoptions="effparam/*.dat --paramname cellsize  --parameval param*1e6 --figs
 
 
 ../../plot_multiline.py $sharedoptions --xlabel "Frequency (THz)" --ycol 'real mu' --y2eval '0-y2' --ycol2 'imag mu' \
-	--ylim1 -5 --ylim2 5 \
-	--paramlabel '%s' --ylabel 'Permeability $\mu_{\text{eff}}$' --output ${PWD##*/}_mu.pdf
+	--ylim1 -10 --ylim2 10 \
+	--paramlabel '$a$ = %.1f $\upmu$m' --ylabel 'Permeability $\mu_{\text{eff}}$' --output ${PWD##*/}_mu.pdf
 ../../plot_multiline.py $sharedoptions --xlabel "Frequency (THz)" --ycol 'real mu' \
 	--ylim1 -5 --ylim2 5 \
-	--paramlabel '%s' --ylabel 'Permeability $\mu_{\text{eff}}^{\prime}$' --output ${PWD##*/}_mur.pdf
+	--paramlabel '$a$ = %.1f $\upmu$m' --ylabel 'Permeability $\mu_{\text{eff}}^{\prime}$' --output ${PWD##*/}_mur.pdf
 ../../plot_multiline.py $sharedoptions --xlabel "Frequency (THz)" --yeval '0-y' --ycol 'imag mu' \
 	--ylim1 -5 --ylim2 5 \
-	--paramlabel '%s' --ylabel 'Permeability $\mu_{\text{eff}}^{\prime\prime}$' --output ${PWD##*/}_mui.pdf
+	--paramlabel '$a$ = %.1f $\upmu$m' --ylabel 'Permeability $\mu_{\text{eff}}^{\prime\prime}$' --output ${PWD##*/}_mui.pdf
