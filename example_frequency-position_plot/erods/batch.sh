ca523decd35904773c0b239062100b2a48b19a53
Filip Dominec <filip.dominec@gmail.com>
2016-02-07 08:05:02 +0100

    generate field data for low-loss structure to enhance contrast

--- example_frequency-position_plot/erods/batch.sh
+++ example_frequency-position_plot/erods/batch.sh
@@ -6,23 +6,23 @@ resolution=2e-6
 model=RodArray
 cellnumber=3
 
-for cellsize in 80e-6 90e-6 120e-6 
+for cellsize in 90e-6 #80e-6 90e-6 120e-6 
 do
 	## Preparation
 	rm -r $model* effparam 2> /dev/null
-	par="simtime=${simtime} cellsize=${cellsize} resolution=${resolution} model=$model orientation=E radius=10e-6 loss=.1 "
+	par="simtime=${simtime} cellsize=${cellsize} resolution=${resolution} model=$model orientation=E radius=10e-6 "
 
 	## Generate the effective parameters
-	mpirun -np $NP ../../scatter.py $par comment=nfile
+	mpirun -np $NP ../../scatter.py $par comment=nfile loss=.1 
 	../../effparam.py
 	 
 	## Generate the field record over multiple cells
-	mpirun -np $NP ../../scatter.py $par comment=fieldevolution cellnumber=${cellnumber} 
+	mpirun -np $NP ../../scatter.py $par comment=fieldevolution cellnumber=${cellnumber}  loss=.001 
 	../../effparam.py
 	 
 	## Plot the results
 	../fxplot.py --nfile effparam/*nfile*dat  --fieldfile `cat last_simulation_name.dat`/FieldEvolution_at_*.h5 \
 		--simtime ${simtime} --cellsize ${cellsize} --resolution ${resolution} --cellnumber ${cellnumber} \
 		--freqlim1 0 --freqlim2 2e12 --numcontours 30 --fieldlim1 55% --fieldlim2 100% --Nconj yes --freqlim1 0.35e12 --freqlim2 1.25e12  \
-		--decimatedata 3 --output erods_fxplot_cs${cellsize}.pdf 
+		--decimatedata 1 --output erods_fxplot_cs${cellsize}_HR.pdf 
 done
