ad2c4268a47e7d3fabc72c1518e01869f271572c
Filip Dominec <filip.dominec@gmail.com>
2016-03-14 11:16:06 +0100

    cleanup

--- tmathieu_gratings/00_test/batch.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-if [ -z $NP ] ; then NP=2 ; fi			 # number of processors
-# par='model=TMathieu_Grating resolution=20n cellsize=100n'
-par='model=TMathieu_Grating resolution=50n padding=10u ldist=15u tdist=10u tshift=3u rcore1=2u rcore2=2u'
-
-mpirun -np $NP   python ../../scatter.py $par simtime=300f
-../../effparam.py 
