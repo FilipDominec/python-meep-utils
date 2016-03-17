9772fa5f86670e9fd9c5623cf75af514d43ebf5a
Filip Dominec <filip.dominec@gmail.com>
2016-03-17 09:31:13 +0100

    fixed against new meep_utils.py

--- cylindrical_cavity.py
+++ cylindrical_cavity.py
@@ -11,13 +11,13 @@ from meep_utils import in_sphere, in_xcyl, in_ycyl, in_zcyl, in_xslab, in_yslab,
 import meep_mpi as meep
 #import meep
 
-sim_param, model_param = meep_utils.process_param(sys.argv[1:])
+model_param = meep_utils.process_param(sys.argv[1:])
 class HollowCyl_model(meep_utils.AbstractMeepModel): #{{{
-    def __init__(self, comment="", simtime=30e-9, resolution=5e-3, cellnumber=1, padding=9e-3, radius=33.774e-3, height=122.36e-3 ,  Kx=0, Ky=0): ## XXXheight=122.3642686e-3
+    def __init__(self, comment="", simtime=30e-9, resolution=5e-3, cellnumber=1, padding=9e-3, radius=33.774e-3, height=122.36e-3 ,  Kx=0, Ky=0, **other_args):
         meep_utils.AbstractMeepModel.__init__(self)        ## Base class initialisation
         self.simulation_name = "HollowCyl"    
         
-        self.register_locals(locals())          ## Remember the parameters
+        self.register_locals(locals(), other_args)          ## Remember the parameters
 
         ## Obligatory parameters (used in the simulation)
         self.pml_thickness = padding/2
@@ -48,64 +48,69 @@ class HollowCyl_model(meep_utils.AbstractMeepModel): #{{{
 #}}}
 
 # Model selection
+model_param = meep_utils.process_param(sys.argv[1:])
 model = HollowCyl_model(**model_param)
-if sim_param['frequency_domain']: model.simulation_name += ("_frequency=%.4e" % sim_param['frequency'])
 
-## Initialize volume and structure according to the model
+## Initialize volume, structure and the fields according to the model
 vol = meep.vol3d(model.size_x, model.size_y, model.size_z, 1./model.resolution)
 vol.center_origin()
-s = meep_utils.init_structure(model=model, volume=vol, sim_param=sim_param, pml_axes="All") ## XXX   meep.XY
-
-## Create the fields object, and define the Bloch-periodic boundaries (any transversal component of k-vector is allowed)
-field = meep.fields(s)
-#field.use_bloch(meep.Z, 1)          ## periodic along the cylinder XXX
+s = meep_utils.init_structure(model=model, volume=vol, pml_axes="All") ## XXX   meep.XY
+f = meep.fields(s)
+# Define the Bloch-periodic boundaries (any transversal component of k-vector is allowed)
+#f.use_bloch(meep.Z, 1)          ## periodic along the cylinder XXX
 # (removing cylinder caps -> making an infinite waveguide with periodic boundary condition, change pml_axes=meep.XY)
 
 # Add the field source (see meep_utils for an example of how an arbitrary source waveform is defined)
-if not sim_param['frequency_domain']:           ## Select the source dependence on time
+if not getattr(model, 'frequency', None):           ## Select the source dependence on time
     #src_time_type = meep.band_src_time(-model.src_freq/c, model.src_width/c, model.simtime*c/1.1)
     src_time_type = meep.gaussian_src_time(-model.src_freq/c, model.src_width/c)  ## negative frequency supplied -> e^(+i omega t) convention
 else:
-    src_time_type = meep.continuous_src_time(-sim_param['frequency']/c) ## TODO check in freq domain that negative frequency is OK, and is it needed?
+    src_time_type = meep.continuous_src_time(-getattr(model, 'frequency', None)/c) ## TODO check in freq domain that negative frequency is OK, and is it needed?
 srcvolume = meep.volume( 
         meep.vec(model.radius*.15, -model.radius*.25, -model.height*.15),
         meep.vec(model.radius*.15, -model.radius*.25, -model.height*.15))
-field.add_volume_source(meep.Ez, src_time_type, srcvolume) ## source of oblique polarization - excites both TE and TM modes
-field.add_volume_source(meep.Ex, src_time_type, srcvolume)
+f.add_volume_source(meep.Ez, src_time_type, srcvolume) ## source of oblique polarization - excites both TE and TM modes
+f.add_volume_source(meep.Ex, src_time_type, srcvolume)
 
-slice_makers =  []
-#slice_makers += [meep_utils.Slice(model=model, field=field, components=(meep.Ex, meep.Ey, meep.Ez), at_t=3, name="ElectricAtEnd")]
-#slice_makers += [meep_utils.Slice(model=model, field=field, components=(meep.Hx, meep.Hy, meep.Hz), at_t=3, name="MagneticAtEnd")]
-#slice_makers =  [meep_utils.Slice(model=model, field=field, components=(meep.Dielectric), at_t=0, name='EPS')]
+slices = []
+if not "noepssnapshot" in model.comment:
+    slices += [meep_utils.Slice(model=model, field=f, components=(meep.Dielectric), at_t=0, name='EPS')]
+if "narrowfreq-snapshots" in model.comment:
+    slices += [meep_utils.Slice(model=model, field=f, components=meep.Ex, at_y=0, at_t=np.inf,
+            name=('At%.3eHz'%getattr(model, 'frequency', None)) if getattr(model, 'frequency', None) else '',
+            outputpng=True, outputvtk=False)]
+if "fieldevolution" in model.comment: 
+    slices += [meep_utils.Slice(model=model, field=f, components=(meep.Ex), at_x=0, name='FieldEvolution', 
+        min_timestep=.1/model.src_freq, outputgif=True, outputvtk=True)]
+if "snapshote" in model.comment:
+    slices += [meep_utils.Slice(model=model, field=f, components=(meep.Ex, meep.Ey, meep.Ez), at_t=np.inf, name='SnapshotE')]
 
-if not sim_param['frequency_domain']:       ## time-domain computation
-    field.step()
-    dt = (field.time()/c)
-    meep_utils.lorentzian_unstable_check_new(model, dt, quit_on_warning=False)
-    timer = meep_utils.Timer(simtime=model.simtime); meep.quiet(True) # use custom progress messages
+
+if not getattr(model, 'frequency', None):       ## time-domain computation
+    f.step(); timer = meep_utils.Timer(simtime=model.simtime); meep.quiet(True) # use custom progress messages
     monitor_point = meep.vec(-model.radius*.5, model.radius*.3, model.height*.3)
     x,y = [], []
-    while (field.time()/c < model.simtime):                               # timestepping cycle
-        field.step()
-        timer.print_progress(field.time()/c)
-        if field.time()/c > 30/model.src_width:
-            x.append(field.time()/c); 
-            y.append(field.get_field(meep.Ex, monitor_point)+field.get_field(meep.Ey, monitor_point)+field.get_field(meep.Ez, monitor_point))
-        for slice_maker in slice_makers: slice_maker.poll(field.time()/c)
-    for slice_maker in slice_makers: slice_maker.finalize()
+    while (f.time()/c < model.simtime):     # timestepping cycle
+        f.step()
+        timer.print_progress(f.time()/c)
+        if f.time()/c > 30/model.src_width:
+            x.append(f.time()/c); 
+            y.append(f.get_field(meep.Ex, monitor_point)+f.get_field(meep.Ey, monitor_point)+f.get_field(meep.Ez, monitor_point))
+        for slice_ in slices: slice_.poll(f.time()/c)
+    for slice_ in slices: slice_.finalize()
     meep_utils.notify(model.simulation_name, run_time=timer.get_time())
 else:                                       ## frequency-domain computation
-    field.step()
-    field.solve_cw(sim_param['MaxTol'], sim_param['MaxIter'], sim_param['BiCGStab']) 
-    for slice_maker in slice_makers: slice_maker.finalize()
+    f.solve_cw(getattr(model, 'MaxTol',0.001), getattr(model, 'MaxIter', 5000), getattr(model, 'BiCGStab', 8)) 
+    for monitor in (monitor1_Ex, monitor1_Hy, monitor2_Ex, monitor2_Hy): monitor.record(field=f)
+    for slice_ in slices: slice_.finalize()
     meep_utils.notify(model.simulation_name)
 
-# Get the reflection and transmission of the structure
-if meep.my_rank() == 0 and  not sim_param['frequency_domain']:
+## Get the resonant field decay waveform inside the cavity
+if meep.my_rank() == 0:
     ## Convert to polar notation and save the time-domain record
     x, y = np.array(x), np.array(y)
     meep_utils.savetxt(fname=model.simulation_name+"_timedomain.dat", X=zip(x, np.abs(y), meep_utils.get_phase(y)), fmt="%.6e",
-            header=model.parameterstring + meep_utils.sim_param_string(sim_param) + "#x-column time [s]\n#column ampli\n#column phase\n")
+            header=model.parameterstring + "#x-column time [s]\n#column ampli\n#column phase\n")
 
     with open("./last_simulation_name.dat", "w") as outfile: outfile.write(model.simulation_name) 
 
