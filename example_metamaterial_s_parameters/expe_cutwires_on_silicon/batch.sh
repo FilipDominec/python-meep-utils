401b316de037ea4039230e2a6716ff6953ba8be8
Filip Dominec <filip.dominec@gmail.com>
2016-08-05 18:58:53 +0200

    np to mpirun

--- example_metamaterial_s_parameters/expe_cutwires_on_silicon/batch.sh
+++ example_metamaterial_s_parameters/expe_cutwires_on_silicon/batch.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-for w in 30 40; do np ../../scatter.py model=WiresOnSi simtime=100p resolution=4u wirelength=${w}u; ../../effparam.py ; done
+
+for w in 30 40; do mpirun -np 2 ../../scatter.py model=WiresOnSi simtime=100p resolution=4u wirelength=${w}u; ../../effparam.py ; done
 
 multiplot --ycol '|t|'  --ylabel 'Transmittance $|t|$'  --ylim2 1.0 --paramname comment --paramlabel %s --figsizey 3 \
 	--xeval x/1e12 --xlim2 3.0  --xlabel "Frequency (THz)" \
