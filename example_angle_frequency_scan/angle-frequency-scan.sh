2a1b11eac67da694c5c9e3e695be4f642ceb29a5
Filip Dominec <filip.dominec@gmail.com>
2017-02-03 14:49:19 +0100

    experience - fix

--- example_angle_frequency_scan/angle-frequency-scan.sh
+++ example_angle_frequency_scan/angle-frequency-scan.sh
@@ -7,23 +7,25 @@ if [ -z $ext ] ; then ext=png ; fi             # number of processors
 
 
 if [ -z "$skipsimulation" ]; then 
-    for K in 0.1 0.3 `seq 0 1 9` `seq 10 2 20`; do    ## transverse wavenumber in 1/um
+    for K in 0.1 0.3 `seq 0 2 9` `seq 12 4 40`; do    ## transverse wavenumber in 1/um
     #for K in 0.1 0.3 `seq 0 1 9` `seq 10 2 20` `seq .5 1 9 | tr , .` `seq 11 2 20`; do    ## transverse wavenumber in 1/um
-        mpirun -np $NP  ../../scatter.py  "${@}" $Kcomponent=${K}e6
+        mpirun -np $NP  ../../scatter.py  "${@}" $Kcomponent=${K}e7
+        #mpirun -np $NP  ../../scatter.py  "${@}" $Kcomponent=${K}e6
     done
 fi
 
 ## Resolve the angle of incidence from the magnitude of the wavevector and its transverse component and plot
-plotoptions=(Half*.dat  --xlabel     'Frequency'   \
+rm last_simulation_name.dat
+plotoptions=(*.dat  --xlabel     'Frequency'   \
         --paramname Kx  --paramlabel 'Angle $a$' --parameval "np.arcsin(param*c/2/np.pi/x)/np.pi*180" \
         --contours yes  --numcontours 25  --colormap gist_earth  --figsizex 4  --figsizey 3 --interp_aspect .5)
 
 ## (place small grey dot at each resolved point for better understanding which points were actually computed)
 ../../plot_multiline.py "${plotoptions[@]}" --ycol '|r|'    --ylabel 'Reflectance   $|r|$'  --ylim1 0 --ylim2 1 \
-        --output ../${PWD##*/}_r.$ext --markersize .5	
+        --output ../${PWD##*/}_r.$ext 
 
 
 ## Clean up
-if [ -z "$skipsimulation" ]; then 
-    rm -r Half*/
-fi
+#if [ -z "$skipsimulation" ]; then 
+    #rm -r ${model}*/
+#fi
