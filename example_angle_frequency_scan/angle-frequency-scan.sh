c11863eec32621bc0ebb44fe3ebcbc47334d3812
Filip Dominec <filip.dominec@gmail.com>
2017-10-11 23:33:14 +0200

    cleaning up tests

--- example_angle_frequency_scan/angle-frequency-scan.sh
+++ example_angle_frequency_scan/angle-frequency-scan.sh
@@ -4,15 +4,14 @@ if [ -z $ext ] ; then ext=png ; fi             # number of processors
 
 if [ -z "$model" ] ; then model=HalfSpace ; fi
 
-## TODO why this is not used in simulation?
-staticpar=(model=$model simtime=25f resolution=150n padding=2.5u  resolution=150n) ## default for the flat air-metal interfaces
+staticpar=(model=$model simtime=50f resolution=100n padding=2.5u  resolution=150n) ## default for the flat air-metal interfaces
 
 
 if [ -z "$skipsimulation" ]; then 
 	## For normal optical simulations
     #for K in 0.1 0.3 ; do #`seq 0 2 9` `seq 12 6 60`; do    ## transverse wavenumber in 1/um 
     for K in 0.1 0.3 `seq 0 2 9` `seq 12 6 60`; do    ## transverse wavenumber in 1/um 
-		#echo "${staticpar[@]}"
+		echo "${staticpar[@]}"
         mpirun -np $NP  ../../scatter.py  "${staticpar[@]}" $Kcomponent=${K}e6
     done
 	## For deep-UV gratings
